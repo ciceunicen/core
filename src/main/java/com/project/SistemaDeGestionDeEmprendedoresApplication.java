84d0590544b96be99693ca0cf7302bfac1fac6e7
johana infesta <johana.infesta@gmail.com>
2022-12-07 19:43:16 -0300

    first commit

---
+++ src/main/java/com/project/SistemaDeGestionDeEmprendedoresApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeEmprendedoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeGestionDeEmprendedoresApplication.class, args);
+    }
+
+}
