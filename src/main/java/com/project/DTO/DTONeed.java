efe60990d289adfd05b44c482fe4c85a082a3666
rocio <rociocpoppe@gmail.com>
2023-01-10 15:48:44 -0300

    se realiza post para una necesidad

---
+++ src/main/java/com/project/DTO/DTONeed.java
@@ -0,0 +1,21 @@
+package com.project.DTO;
+
+public class DTONeed {
+    
+    private Long id_Need;
+    private String needType;
+    
+    public Long getId_Need() {
+        return id_Need;
+    }
+    public void setId_Need(Long id_Need) {
+        this.id_Need = id_Need;
+    }
+    public String getNeedType() {
+        return needType;
+    }
+    public void setNeedType(String needType) {
+        this.needType = needType;
+    }
+
+}
