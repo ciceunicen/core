b05b1c762e56bead75ef49cb0440b8b4be94977f
Lautaro Sanchez <lautasanchez11@gmail.com>
2023-04-12 12:07:11 -0300

    modificando la clase ApplicationSecurityConfig

--- src/main/java/com/project/ApplicationSecurityConfig.java
+++ src/main/java/com/project/ApplicationSecurityConfig.java
@@ -10,10 +10,52 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	
 
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-        http.authorizeRequests().anyRequest().permitAll();
+	@Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+             
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepo.findByEmail(username)
+                        .orElseThrow(
+                                () -> new UsernameNotFoundException("User " + username + " not found"));
+            }
+        };
+    }
+ 	
+ 	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+ 	
+ 	@Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+ 	
+ 	@Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+         
+        http.authorizeRequests()
+        .antMatchers(HttpMethod.POST,"/usuarios").permitAll()
+        .antMatchers("/auth/login").permitAll()
+        .anyRequest().authenticated();
+         
+            http.exceptionHandling()
+                    .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            response.sendError(
+                                HttpServletResponse.SC_UNAUTHORIZED,
+                                ex.getMessage()
+                            );
+                        }
+                );
+         
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+         
+        return http.build();
     }
 }
