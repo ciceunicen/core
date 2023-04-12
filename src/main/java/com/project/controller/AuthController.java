f9d137abf54f21d0bfa078e03005560e603a76a1
Lautaro Sanchez <lautasanchez11@gmail.com>
2023-04-12 12:33:35 -0300

    agregado authController

---
+++ src/main/java/com/project/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.project.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import com.project.POJO.AuthRequest;
+import com.project.POJO.AuthResponse;
+import com.project.entities.User;
+import com.project.jwt.JwtTokenUtil;
+
+@RestController
+public class AuthController {
+
+	@Autowired AuthenticationManager authManager;
+    @Autowired JwtTokenUtil jwtUtil;
+     
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request) {
+        try {
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(), request.getPassword())
+            );
+             
+            User user = (User) authentication.getPrincipal();
+            String accessToken = jwtUtil.generateAccessToken(user);
+            AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
+             
+            return ResponseEntity.ok().body(response);
+             
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
