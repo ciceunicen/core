18a3d1b37c474bf723977da30769d0af731d98f4
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2023-04-20 19:58:49 -0300

    Se realizo una query para si ya existe el email en db

--- src/main/java/com/project/repository/UserRepository.java
+++ src/main/java/com/project/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.project.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.entities.User;
@@ -10,4 +11,8 @@ import com.project.entities.User;
 public interface UserRepository extends JpaRepository<User,Long> {
 	
 	Optional<User> findByEmail(String email);
-}
+	
+	@Query(value="SELECT u.email FROM user u WHERE u.email=:email", nativeQuery = true)
+	public String isEmail( String email);
+	
+	}
