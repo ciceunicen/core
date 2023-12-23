c9191e42ed600f94a967b62dbd9bcb430384a481
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-12-23 17:30:14 -0300

    Devuelve las notificaciones ordenadas por fecha

--- src/main/java/com/project/repository/NotificationRepository.java
+++ src/main/java/com/project/repository/NotificationRepository.java
@@ -14,12 +14,14 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 
 	@Query("SELECT n"
 			+ " FROM Notification n"
-			+ " WHERE n.administrator = :administrator")
+			+ " WHERE n.administrator = :administrator"
+			+ " ORDER BY n.date DESC")
 	public List<Notification> findAllByAdministrator(User administrator);
 	
 	@Query("SELECT n"
 			+ " FROM Notification n"
 			+ " WHERE n.administrator = :administrator"
-			+ " AND n.isRead IS NOT TRUE")
+			+ " AND n.isRead IS NOT TRUE"
+			+ " ORDER BY n.date DESC")
 	public List<Notification> findAllByNotReadAndAdministrator(User administrator);
 }
