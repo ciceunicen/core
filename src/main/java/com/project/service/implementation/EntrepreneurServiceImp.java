2b8af51b354bfc53b8f9c43fdedaa3235295aec2
Matias Sciancalepore <msciancalepore47@gmail.com>
2024-06-04 17:16:46 -0300

    agrego atributo is_aceptado en el DTOEntrepreneur y agrego constructor para poder pasarlo

--- src/main/java/com/project/service/implementation/EntrepreneurServiceImp.java
+++ src/main/java/com/project/service/implementation/EntrepreneurServiceImp.java
@@ -215,7 +215,7 @@ public class EntrepreneurServiceImp  implements EntrepreneurService{
 		for (Entrepreneur e: entrepreneurs) {
 			if(!e.getAceptada()){
 			DTOEntrepreneur dto = new DTOEntrepreneur(e.getId(), e.getDni(), e.getName(), e.getSurname(), e.getEmail(), e.getId_user(),
-					e.getIs_active(), e.getCuil_cuit(), e.getPhone(), e.getLocation(), e.getHowimetcice(), e.isIspf(), e.is_deleted());
+					e.getIs_active(), e.getCuil_cuit(), e.getPhone(), e.getLocation(), e.getHowimetcice(), e.isIspf(), e.is_deleted(),e.getAceptada());
 			listaDTO.add(dto);
 			}
 		}
